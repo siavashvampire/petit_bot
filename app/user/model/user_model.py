86db14a1c7f0b0adc61654567ccdacbd0b37a1b9
siavash sepahi <sepahisiavash@gmail.com>
2022-05-25 18:21:39 +0430

    add accounting_flag to user

--- app/user/model/user_model.py
+++ app/user/model/user_model.py
@@ -12,7 +12,7 @@ class UserDB(User):
     idea_flag: bool
 
     def __init__(self, id_in: int = 0, username: str = "", first_name: str = "",
-                 last_name: str = "", idea_flag: bool = False):
+                 last_name: str = "", idea_flag: bool = False, accounting_flag: bool = False):
         self.table = table
 
         if id_in != 0 and username == "":
@@ -21,14 +21,17 @@ class UserDB(User):
             username = search['username']
             first_name = search['first_name']
             self.idea_flag = search['idea_flag']
+            self.accounting_flag = search['accounting_flag']
         else:
             self.idea_flag = idea_flag
+            self.accounting_flag = accounting_flag
 
         super().__init__(id=id_in, first_name=first_name, is_bot=False, last_name=last_name, username=username)
 
     def insert_user(self):
         self.table.upsert(
-            {'id': self.id, 'username': self.username, 'first_name': self.first_name, 'idea_flag': self.idea_flag},
+            {'id': self.id, 'username': self.username, 'first_name': self.first_name,
+             'idea_flag': self.idea_flag, 'accounting_flag': self.accounting_flag},
             query.id == self.id)
 
     def is_idea_admin(self):
@@ -41,5 +44,13 @@ class UserDB(User):
             return True
         return False
 
+    def can_insert_accounting(self):
+        if self.idea_flag:
+            return True
+        return False
+
     def chang_idea_flag(self, value: bool):
         self.table.update({'idea_flag': value}, query.id == self.id)
+
+    def chang_accounting_flag(self, value: bool):
+        self.table.update({'accounting_flag': value}, query.id == self.id)
