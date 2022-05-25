e03e94ce6ad95a57ddc7796207414979e480f6ba
siavash sepahi <sepahisiavash@gmail.com>
2022-05-25 18:47:10 +0430

    add set command_upload_picture and upload pdf

--- core/handler.py
+++ core/handler.py
@@ -1,6 +1,7 @@
 from telegram.update import Update
 from telegram.ext.callbackcontext import CallbackContext
 
+from app.accounting.main import set_accounting_upload_picture, set_accounting_upload_pdf_file
 from app.density.main import density_set_density
 from app.idea.main import idea_stl_file_question, set_idea_accepted_idea, set_idea_same_innovator, \
     set_idea_input_description, set_idea_input_innovator, set_idea_upload_picture, set_idea_change_user_idea_start, \
@@ -18,7 +19,6 @@ def button(update: Update, context: CallbackContext) -> None:
         set_idea_same_innovator(update, context)
     elif context.chat_data['command'] == 'set_idea_change_user_idea':
         set_idea_change_user_idea(update, context)
-
 
 
 def input_text(update: Update, context: CallbackContext) -> None:
@@ -45,6 +45,21 @@ def readfile_png(update: Update, context: CallbackContext):
         return
     elif chat_data['command'] == 'set_idea_upload_picture':
         set_idea_upload_picture(update, context)
+
+    elif chat_data['command'] == 'set_accounting_upload':
+        set_accounting_upload_picture(update, context)
+    else:
+        update.message.reply_text("command that set is wrong")
+        return
+
+
+def pdf_handler(update: Update, context: CallbackContext):
+    chat_data = context.chat_data
+    if 'command' not in chat_data.keys() or chat_data['command'] == '':
+        update.message.reply_text("command not set")
+        return
+    elif chat_data['command'] == 'set_accounting_upload':
+        set_accounting_upload_pdf_file(update, context)
     else:
         update.message.reply_text("command that set is wrong")
         return
