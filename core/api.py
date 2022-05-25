b3384280ec8ac19d5c06cf0d026a187a7d4709b2
siavash sepahi <sepahisiavash@gmail.com>
2022-05-25 18:47:22 +0430

    cleaning

--- core/api.py
+++ core/api.py
@@ -6,5 +6,5 @@ from MainCode import parent_path
 def download_photo(photo: PhotoSize, path: str):
     files = photo.get_file()
     path_temp = parent_path.joinpath(path)
-    f_gcode = files.download(str(path_temp))
-    return f_gcode
+    file = files.download(str(path_temp))
+    return file
