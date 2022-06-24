ba6a971ef18c3ca3f9f52f70d0f3c31fa4d0c973
siavash sepahi <sepahisiavash@gmail.com>
2022-06-24 15:51:11 +0430

    add type hint

--- core/model/Node_Model.py
+++ core/model/Node_Model.py
@@ -1,15 +1,13 @@
-from typing import Callable
+from typing import Callable, Optional
 
 from anytree import RenderTree, NodeMixin
 from anytree.node.util import _repr
 
 
-class Node(NodeMixin):
-    # children: tuple[Node]
-    # parent: Optional[Node]
+class NodeCore(NodeMixin):
     name: str
 
-    def __init__(self, name: str, function: Callable = None, parent=None, children=None):
+    def __init__(self, name: str, function: Callable = None, parent: NodeMixin = None, children=None):
         self.name = name
 
         self.parent = parent
@@ -39,3 +37,34 @@ class Node(NodeMixin):
 
     def render(self):
         print(RenderTree(self))
+
+
+class Node(NodeCore):
+    children: tuple[NodeCore]
+    parent: Optional[NodeCore]
+    name: str
+
+    def __init__(self, name: str, function: Callable = None, parent: NodeCore = None, children: tuple[NodeCore] = None):
+        super().__init__(name, function, parent, children)
+        self.name = name
+
+        self.parent = parent
+
+        self.function = function
+
+        if children:
+            self.children = children
+
+    def get_child_by_id(self, id_in: int) -> Optional[NodeCore]:
+        if id_in < len(self.children):
+            return self.children[id_in]
+
+        print(f'child with id {id_in} not found!!')  # TODO:bayad y exception ro ham seda bezane
+        return None
+
+    def get_child_by_name(self, name: str) -> Optional[NodeCore]:
+        for child in self.children:
+            if child.name == name:
+                return child
+        print(f'child with name {name} not found!!')  # TODO:bayad y exception ro ham seda bezane
+        return None
